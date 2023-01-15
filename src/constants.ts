fa49f898a1fd538d5409a583d5697fca73009032
BuildTools <kareemfakhuri@gmail.com>
2023-01-15 18:16:44 -0500

    pushed missing poetry configs to npm

--- src/constants.ts
+++ src/constants.ts
@@ -4,12 +4,9 @@ const SCRIPT_NAME = "score_collection.py";
 const DUMP_NAME = "metadata.json";
 const SCORES_NAME = "scores.json";
 
-export const SCRIPT_PATH = path.join(
-  path.normalize(path.join(__dirname, "..")),
-  "scripts",
-  SCRIPT_NAME
-);
+const BASE_PATH = path.normalize(path.join(__dirname, ".."));
 
-export const DUMP_FOLDER = path.join(process.cwd(), "dumps");
+export const SCRIPT_PATH = path.join(BASE_PATH, "scripts", SCRIPT_NAME);
+export const DUMP_FOLDER = path.join(BASE_PATH, "dumps");
 export const METADATA_PATH = path.join(DUMP_FOLDER, DUMP_NAME);
 export const SCORES_PATH = path.join(DUMP_FOLDER, SCORES_NAME);
